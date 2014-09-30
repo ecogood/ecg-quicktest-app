8ecc51e188598c6e5c1fa9bb00913e848d47aedc
Nikolay Georgiev <nikolay@georgiev.cc>
2014-09-30 19:50:19 +0200

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ecgQuicktestApp
+ * @description
+ * # ecgQuicktestApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ecgQuicktestApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+    /*
+     * / - main page
+     * /test - enter company name and type
+     * /test/1
+     * /test/2
+     * /test/...
+     * /test/27
+     * /test/result - show results, btn "start a new test"
+     *
+     */
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ecgQuicktestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ecgQuicktestApp
+ */
+angular.module('ecgQuicktestApp')
+  .controller('MainCtrl', function($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
