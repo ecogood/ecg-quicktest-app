6c02994574811fee3c4f9a12d644538b5280f91d
Nikolay Georgiev <nikolay@georgiev.io>
2014-10-09 15:18:58 +0200

    feat(services, templates, ) - first working version

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,5 +8,25 @@
  * Controller of the ecgQuicktestApp
  */
 angular.module('ecgQuicktestApp')
-  .controller('HomeCtrl', function() {
+  .controller('HomeCtrl', function($scope, $log, $location, ecgQuicktestModel) {
+    $scope.readMore = false;
+    $scope.$log = $log;
+
+    $scope.participantType = 'company';
+    $scope.isSelfEmployed = false;
+    $scope.setSelfEmployed = function() {
+      if ($scope.isSelfEmployed === 'true') {
+        $scope.participantType = 'self-employed';
+      } else {
+        $scope.participantType = 'company';
+      }
+    };
+
+    $scope.startTest = function(){
+      var test = ecgQuicktestModel.factory();
+      test.setParticipantType($scope.participantType);
+      test.getParticipant().name = $scope.participantName;
+      $scope.$parent.test = test;
+      $location.path('test/1');
+    };
   });
